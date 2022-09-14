493eb9149c8adea71694b5a8081f9285d16173ca
Luis Nunes <luisnunes.al@gmail.com>
2022-09-14 17:23:37 -0300

    transformando o projeto em typescript e ajustes finais

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     presets: [
         '@babel/preset-env',
+        '@babel/preset-typescript',
         ['@babel/preset-react', {
             runtime: 'automatic' //para nao precisar importar React em todo arq
         }]
--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot  } from 'react-dom/client';
-import { App } from './App';
-
-const root = createRoot(document.getElementById('root')); // createRoot(container!) if you use TypeScript
-root.render(<App/>);
